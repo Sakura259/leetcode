f4c342f4c1dec80ac6771453a1ad8153d56774cb
皓白 <sakuracjx259@gmail.com>
2021-02-19 20:17:55 +0800

    init

--- src/main/java/com/haobai/leetcode/back/目标和.java
+++ src/main/java/com/haobai/leetcode/back/目标和.java
@@ -1,5 +1,8 @@
 package com.haobai.leetcode.back;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author haobai
  * @description: 494.目标和
@@ -28,9 +31,24 @@ public class 目标和 {
 
     private int result = 0;
 
+    public static void main(String[] args) {
+        System.out.println(new 目标和().findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
+    }
+
     public int findTargetSumWays(int[] nums, int S) {
-        backTrack(0, nums, S);
-        return result;
+        // 回溯的解法
+//        backTrack(0, nums, S);
+//        return result;
+        // 备忘录的解法
+//        return dp(nums, 0, S);
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+        return subSets(nums, (sum + S) / 2);
     }
 
     /**
@@ -56,6 +74,48 @@ public class 目标和 {
         target -= nums[i];
         backTrack(i + 1, nums, target);
         // 这边不用撤销，不会在进行判断了
+    }
+
+    private Map<String, Integer> memo = new HashMap<>(16);
+
+    /**
+     * 备忘录的形式来消除重叠问题
+     *
+     * @param nums
+     * @param i
+     * @param target
+     * @return
+     */
+    private int dp(int[] nums, int i, int target) {
+        if (i == nums.length) {
+            if (target == 0) {
+                return 1;
+            }
+            return 0;
+        }
+        String key = i + "," + target;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        int result = dp(nums, i + 1, target - nums[i]) + dp(nums, i + 1, target + nums[i]);
+        memo.put(key, result);
+        return result;
+    }
+
+    private int subSets(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < target + 1; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[j] = dp[j] + dp[j - nums[i - 1]];
+                } else {
+                    dp[j] = dp[j];
+                }
+            }
+        }
+        return dp[target];
     }
 
 
