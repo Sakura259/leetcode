f374e5843b96bed572bb6314e7fd3a56ff8de029
皓白 <sakuracjx259@gmail.com>
2021-02-25 20:48:15 +0800

    5道题

---
+++ src/main/java/com/haobai/niuke/NC32_求平方根.java
@@ -0,0 +1,18 @@
+package com.haobai.niuke;
+
+/**
+ * @author haobai
+ * @description:
+ * @date 2021/2/25 16:17
+ */
+public class NC32_求平方根 {
+
+    /**
+     *
+     * @param x int整型
+     * @return int整型
+     */
+    public int sqrt (int x) {
+        return (int) Math.sqrt(x);
+    }
+}
